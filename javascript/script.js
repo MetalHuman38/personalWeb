2dd68f3e1a1d4a4d63b3cf4e4b373cf90e6a9542
MetalHuman38 <106476905+MetalHuman38@users.noreply.github.com>
2023-05-23 23:36:29 +0100

    Adding placeholder

--- javascript/script.js
+++ javascript/script.js
@@ -1,12 +1,48 @@
-const heroImage = document.querySelector(".hero-image");
+let heroImage = document.querySelector(".hero-image");
 
-heroImage.addEventListener("mouseenter", () => {
-  heroImage.classList.add("animate");
-});
+if (heroImage !== null) {
+  heroImage.addEventListener("mouseenter", function () {
+    heroImage.classList.add("animate");
+  });
 
-heroImage.addEventListener("mouseleave", () => {
-  heroImage.classList.remove("animate");
-});
+  heroImage.addEventListener("mouseleave", function () {
+    heroImage.classList.remove("animate");
+  });
+}
 
 const currentYear = new Date().getFullYear();
-document.getElementById("current-year").textContent = currentYear;
+let currentYearElement = document.getElementById("current-year");
+if (currentYearElement !== null) {
+  currentYearElement.textContent = currentYear;
+}
+
+let magnifier = document.getElementById("custom-cursor");
+
+if (magnifier !== null) {
+  document.addEventListener("mousemove", function (e) {
+    magnifier.style.left = e.pageX + "px";
+    magnifier.style.top = e.pageY + "px";
+    magnifier.style.backgroundPosition =
+      "-" + (e.pageX - 50) + "px -" + (e.pageY - 50) + "px";
+
+    // If the mouse is over an element, expand the magnifier
+    if (document.elementFromPoint(e.clientX, e.clientY) !== document.body) {
+      magnifier.style.width = "100px";
+      magnifier.style.height = "100px";
+    } else {
+      // If the mouse is not over an element, reduce the size of the magnifier
+      magnifier.style.width = "50px";
+      magnifier.style.height = "50px";
+    }
+  });
+}
+
+$(function () {
+  $("#header-placeholder").load("/header.html");
+});
+
+$(function () {
+  $("#footer-placeholder").load("/footer.html");
+});
+
+alert($().jquery);
