170dcfa59f4e86ab68ad91ae30659bb81576a908
MetalHuman38 <106476905+MetalHuman38@users.noreply.github.com>
2023-05-27 04:30:15 +0100

    Tailwindv3

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./thinkAlike/myFlaskapp/templates/**/*.html'",
+    "./thinkAlike/myFlaskapp/static/javascript/script.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
